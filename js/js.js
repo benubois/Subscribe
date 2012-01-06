7a5f24b8454e6c8b9c92274795e915b2526a9343
Ben Ubois <bb@flickerbox.com>
2012-01-06 02:54:40 -0800

    Added content.

--- js/js.js
+++ js/js.js
@@ -1,3 +1,23 @@
 if (document.location.host == 'benubois.github.com' || document.location.host == 'benubois.com') {
 	window.location = 'http://subscribeapp.com'
 }
+window.subscribe = {};
+subscribe.init = {
+	trackLink: function () {
+		$('.ga-track').on('click', function (e) {
+			_gaq._trackEvent('Outbound Links', $(this).prop('href'));
+			setTimeout(function () {
+				
+				// document.location = $(this).prop('href');
+			}, 100);
+			return false;
+		});
+	}
+}
+$(document).ready(function() {
+	$.each(subscribe.init, 
+		function(i,item) {
+			item();
+		}
+	);
+});
